6737826588e07fff5bfb2250a07e1e7f641b8c3b
Luis Henrique Rocha Barros <luisrochabarros@hotmail.com>
2023-06-01 20:56:43 -0300

    Desabilitando JWT para testes

--- src/main/java/br/com/neosaude/api/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/neosaude/api/infra/security/SecurityConfigurations.java
@@ -28,6 +28,7 @@ public class SecurityConfigurations {
                 .and().authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, "/login").permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                .requestMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and().cors()
                 .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
